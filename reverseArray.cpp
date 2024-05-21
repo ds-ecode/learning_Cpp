690ac801878e6b15f1cd17352d65e5a7938d6901
Divyansh Sharma <158248079+ds-ecode@users.noreply.github.com>
2024-05-21 13:15:04 +0530

    Create reverseArray.cpp

---
+++ reverseArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int swap(int arr[],int a,int b){
+    int temp =arr[a];
+    arr[a]=arr[b];
+    arr[b]=temp;
+    return(0);
+}
+
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<(size/2);i++){
+        swap(arr,i,size-1-i);
+    }
+    
+    for(int j=0;j<size;j++){
+        cout<<arr[j]<<" ";
+    }
+
+    return 0;
+}
