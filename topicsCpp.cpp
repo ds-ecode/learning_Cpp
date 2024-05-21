084585a8671f5e0d917f1ad83b7e23705841f93c
Divyansh Sharma <158248079+ds-ecode@users.noreply.github.com>
2024-05-21 18:36:54 +0530

    array shhet by yashi mam

---
+++ topicsCpp.cpp
@@ -0,0 +1,47 @@
+/*>
+1
+Write a program to Find the occurrence of an integer in the array?
+Input: N = 7, X = 2, Arr[] = {1, 1, 2, 2, 2, 2, 3}
+Output: 4
+
+
+Q2->Write a program to cyclically rotate the array clockwise by one time.?
+Input: arr[] = {1, 2, 3, 4, 5}
+Output: arr[] = {5, 1, 2, 3, 4}
+
+
+Q3->Write a program to find the minimum and maximum elements in an array?
+Inputs : 1 423 6 46 34 23 13 53 4
+Output : min-1 max-423
+
+
+Q4-> Write a program to reverse an array?
+Inputs : 1 2 3 4 5
+Output : 5 4 3 2 1
+
+
+Q5 -> Write a program to separate even and odd numbers in an array of integers.
+Put all even numbers first, and then odd numbers.?
+Original array: 0 1 3 4 5 6 7 8 10
+Array after divided: 0 10 8 4 6 5 7 3 1
+
+
+Q6-> Write a program to sort a given array of 0s, 1s and 2s. In the final array put
+all 0s first, then all 1s and all 2s last.
+Original array: 0 1 2 2 1 1 0 0 1
+Array elements after rearranging: 0 0 0 1 1 1 1 2 2
+
+
+Q7-> Write a program to find a number that occurs an odd number of times in a
+given array of positive integers. In the array, all numbers occur an even number
+of times.
+Original array: 5 7 8 8 5 8 8 7 7
+Number which occurs odd number of times: 7
+
+
+Q8-> Find duplicates in O(n) time and O(1) extra space Given an array of n
+elements that contains elements from 0 to n-1, with any of these numbers
+appearing any number of times. Find these repeating numbers in O(n) and
+use only constant memory space.
+Input: n=7 , array[]={1, 2, 3, 6, 3, 6, 1}
+Output: 1, 3, 6*/
