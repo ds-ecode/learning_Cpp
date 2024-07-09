f33802eba1095d25112359281f101a9da52c1f25
Divyansh Sharma <158248079+ds-ecode@users.noreply.github.com>
2024-07-09 20:42:21 +0530

    Create Area_Rect.c

---
+++ c/Area_Rect.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(){
+    // Area of Rectangle
+    float l=3.2;
+    float b=4.0;
+    float area = l*b;
+    printf("The Length of the rectangle is: %f\n",l);
+    printf("The Breadth of the rectangle is: %f\n",b);
+    printf("The Area of the rectangle is: %f",area);
+    return 0;
+}
